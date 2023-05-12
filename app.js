851cc52d30b8fdc6e973862ca59c80a9d795dca2
Ansh <anshg1921@gmail.com>
2023-05-13 00:25:09 +0530

    added swagger ui

--- app.js
+++ app.js
@@ -9,6 +9,11 @@ const cors = require('cors');
 const xss = require('xss-clean');
 const rateLimiter = require('express-rate-limit');
 
+//swagger
+const swaggerUI = require('swagger-ui-express');
+const YAML = require('yamljs');
+const swaggerDocument = YAML.load('./swagger.yaml');
+
 // error handler
 const notFoundMiddleware = require('./middleware/not-found');
 const errorHandlerMiddleware = require('./middleware/error-handler');
@@ -35,8 +40,10 @@ app.use(xss());
 // extra packages
 
 app.get('/', (req, res) => {
-	res.send('jobs api');
+	res.send('<h1>Jobs API</h1><a href="/api-docs">Documentation</a>');
 });
+
+app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocument));
 
 // routes
 app.use('/api/v1/auth', authRouter);
